1283bf496d1137ff4518e685c32b1980eb9ad186
Aaron McLeod <aaron.g.mcleod@gmail.com>
2018-12-03 20:00:01 -0500

    day 3 done

--- 3/main.rs
+++ 3/main.rs
@@ -1,10 +1,28 @@
 extern crate read_input;
 
+use std::collections::{HashMap};
+use std::cmp;
+
+#[derive(Debug)]
 struct Vector2 {
     x: usize,
     y: usize,
 }
 
+#[derive(Debug)]
+struct SquareInch {
+    ids: Vec<usize>,
+}
+
+impl SquareInch {
+    fn new() -> Self {
+        SquareInch{
+            ids: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Claim {
     id: usize,
     pos: Vector2,
@@ -40,5 +58,48 @@ fn main() {
         Claim::from_text(i + 1, line)
     }).collect();
 
-    println!("{}", claims.len());
+    let mut fabric: HashMap<(usize, usize), SquareInch> = HashMap::new();
+    let mut count = 0;
+    for (i, claim) in claims.iter().enumerate() {
+        let id = i + 1;
+        // iterate through the square
+        for x in claim.pos.x..(claim.pos.x + claim.size.x) {
+            for y in claim.pos.y..(claim.pos.y + claim.size.y) {
+                let mut si = if fabric.contains_key(&(x, y)) {
+                    fabric.get_mut(&(x, y)).unwrap()
+                } else {
+                    fabric.insert((x, y), SquareInch::new());
+                    fabric.get_mut(&(x, y)).unwrap()
+                };
+
+                // store this id in the location
+                si.ids.push(id);
+                if si.ids.len() == 2 {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", count);
+
+    // store the highest count for each id, across their squares.
+    // As some can have a count of 1, and others a count of 2 for the same ID
+    let result = fabric.iter().fold(HashMap::new(), |mut obj, (_, square_inch)| {
+        for id in &square_inch.ids {
+            if obj.contains_key(&id) {
+                let mut count = obj.get_mut(&id).unwrap();
+                *count = cmp::max(*count, square_inch.ids.len());
+            } else {
+                obj.insert(id, square_inch.ids.len());
+            }
+        }
+        obj
+    });
+
+    for (id, value) in &result {
+        if *value == 1 {
+            println!("{}", id);
+        }
+    }
 }
