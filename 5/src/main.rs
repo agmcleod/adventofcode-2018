7aa63c80b98f305e839473d50f1f4446e6a38bc7
Aaron McLeod <aaron.g.mcleod@gmail.com>
2018-12-05 20:13:18 -0500

    finished day5

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,9 +1,9 @@
 extern crate read_input;
 
-fn main() {
-    let text = read_input::read_text("5/input.txt").unwrap();
-    let mut letters: Vec<&str> = text.split("").filter(|v| *v != "").collect();
+use std::collections::HashSet;
+use std::cmp;
 
+fn apply_polymers(mut letters: Vec<&str>) -> usize {
     let mut index_to_drop = 0;
     'main: loop {
         let mut found = false;
@@ -23,10 +23,42 @@ fn main() {
         if found {
             letters.remove(index_to_drop);
             letters.remove(index_to_drop);
+            // remove one so we skip to the right spot
+            if index_to_drop > 0 {
+                index_to_drop -= 1;
+            }
         } else {
             break 'main
         }
     }
 
-    println!("{}", letters.len());
+    letters.len()
+}
+
+fn main() {
+    let text = read_input::read_text("5/input.txt").unwrap();
+    let mut letter_set = HashSet::new();
+    let letters: Vec<&str> = text.split("").filter(|v| *v != "" && *v != "\n").map(|letter| {
+        letter_set.insert(letter.to_lowercase());
+        letter
+    }).collect();
+
+    println!("{}", apply_polymers(letters.clone()));
+
+    let mut length = None;
+    for letter_to_erase in &letter_set {
+        let filtered_letters: Vec<&str> = letters
+            .iter()
+            .filter(|letter| *letter != letter_to_erase && *letter != &letter_to_erase.to_uppercase())
+            .cloned().collect();
+
+        let res = apply_polymers(filtered_letters);
+        if length.is_some() {
+            length = Some(cmp::min(length.unwrap(), res));
+        } else {
+            length = Some(res);
+        }
+    }
+
+    println!("{}", length.unwrap());
 }
