507c2d4e5690fc219de2467a54dcd1f1c761f6b7
Aaron McLeod <aaron.g.mcleod@gmail.com>
2018-12-12 23:13:51 -0500

    optimal solution for day11p2

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -4,38 +4,67 @@ use std::cmp;
 fn get_power(x: i32, y: i32) -> i32 {
     let rack_id = x + 10;
     let result = (rack_id * y + 9435) * rack_id;
-    if result < 100 {
+    let power = if result < 100 {
         0
     } else {
         format!("{}", result).rsplit("").nth(3).unwrap().parse().unwrap()
+    };
+
+    power - 5
+}
+
+fn create_sum_area_table(size: i32) -> HashMap<(i32, i32), i32> {
+    let mut sum_area_table = HashMap::new();
+    for x in 1..=size {
+        for y in 1..=size {
+            let sum = get_power(x, y) +
+                get_value_to_add(&sum_area_table, x, y - 1) +
+                get_value_to_add(&sum_area_table, x - 1, y) -
+                get_value_to_add(&sum_area_table, x - 1, y - 1);
+            sum_area_table.insert(
+                (x, y),
+                sum
+            );
+        }
+    }
+
+    sum_area_table
+}
+
+fn get_value_to_add(sum_area_table: &HashMap<(i32, i32), i32>, x: i32, y: i32) -> i32 {
+    if sum_area_table.contains_key(&(x, y)) {
+        *sum_area_table.get(&(x, y)).unwrap()
+    } else {
+        0
     }
 }
 
-fn sol(fuel_cell_totals: &mut HashMap<(i32, i32), i32>, grid_size: i32) -> (i32, (i32, i32)) {
+fn sum_for_grid(sum_area_table: &HashMap<(i32, i32), i32>, grid_size: i32, x: i32, y: i32) -> i32 {
+    let g = grid_size - 1;
+    get_value_to_add(sum_area_table, x + g, y + g) +
+        get_value_to_add(sum_area_table, x - 1, y - 1) -
+        get_value_to_add(sum_area_table, x + g, y - 1) -
+        get_value_to_add(sum_area_table, x - 1, y + g)
+}
+
+fn sol(sum_area_table: &HashMap<(i32, i32), i32>, grid_size: i32) -> (i32, (i32, i32)) {
     let mut sum_with_coords = HashMap::new();
 
     let mut highest = 0;
+    sum_with_coords.insert(0, (-1, -1));
 
-    for x in 0..=(300 - grid_size + 1) {
-        for y in 0..=(298 - grid_size + 1) {
-
-            let mut sum = 0;
-            for x in x..=(x + grid_size - 1) {
-                for y in y..=(y + grid_size - 1) {
-                    let power = if fuel_cell_totals.contains_key(&(x, y)) {
-                        *fuel_cell_totals.get(&(x, y)).unwrap()
-                    } else {
-                        let power = get_power(x, y);
-                        fuel_cell_totals.insert((x, y), power);
-                        power
-                    };
-
-                    sum += power;
-                }
+    // case for handling 300x300
+    if grid_size == 300 {
+        let sum = sum_for_grid(sum_area_table, grid_size, 1, 1);
+        highest = cmp::max(sum, highest);
+        sum_with_coords.insert(sum, (1, 1));
+    } else {
+        for x in 1..=(300 - grid_size) {
+            for y in 1..=(300 - grid_size) {
+                let sum = sum_for_grid(sum_area_table, grid_size, x, y);
+                highest = cmp::max(sum, highest);
+                sum_with_coords.insert(sum, (x, y));
             }
-
-            highest = cmp::max(sum, highest);
-            sum_with_coords.insert(sum, (x, y));
         }
     }
 
@@ -43,8 +72,9 @@ fn sol(fuel_cell_totals: &mut HashMap<(i32, i32), i32>, grid_size: i32) -> (i32,
 }
 
 fn main() {
-    let mut fuel_cell_totals = HashMap::new();
-    let part_one = sol(&mut fuel_cell_totals, 3);
+    let sum_area_table = create_sum_area_table(300);
+
+    let part_one = sol(&sum_area_table, 3);
 
     println!("{} {:?}", part_one.0, part_one.1);
 
@@ -52,11 +82,7 @@ fn main() {
     let mut highest_sum_grid_size = 3;
     let mut highest_coord = (0, 0);
     for size in 1..=300 {
-        if size == 3 {
-            continue
-        }
-
-        let result = sol(&mut fuel_cell_totals, size);
+        let result = sol(&sum_area_table, size);
         if result.0 > highest_sum {
             highest_sum = result.0;
             highest_sum_grid_size = size;
