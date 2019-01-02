03f201a2a86796dc1f67af1103d29d34675cacdf
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-01-01 19:59:13 -0500

    First tried DFS, but p2 wasnt working BFS didnt change answer, but figured out to check each coord in paths past 1k

--- 20/src/main.rs
+++ 20/src/main.rs
@@ -98,23 +98,10 @@ fn draw_map(map: &HashMap<Coord, Room>, min_x: i32, min_y: i32, max_x: i32, max_
     }
 }
 
-fn main() {
-    let text = read_input::read_text("20/example2.txt").unwrap();
-    let text = text.replace("^", "").replace("$", "");
-
-    let mut branching_coords: Vec<Coord> = Vec::new();
-
-    let mut map: HashMap<Coord, Room> = HashMap::new();
-
+fn setup_map(text: &String, map: &mut HashMap<Coord, Room>, min_x: &mut i32, min_y: &mut i32, max_x: &mut i32, max_y: &mut i32) {
     let mut coord: Coord = (0, 0);
     map.insert(coord, Room::new(coord));
-
-    let mut min_x = 0;
-    let mut min_y = 0;
-
-    let mut max_x = 0;
-    let mut max_y = 0;
-
+    let mut branching_coords: Vec<Coord> = Vec::new();
     for (i, ch) in text.chars().enumerate() {
         if ch == '(' {
             branching_coords.push(coord);
@@ -148,14 +135,92 @@ fn main() {
                 let room = map.get_mut(&coord).unwrap();
                 room.doors.insert(direction);
 
-                min_x = cmp::min(min_x, coord.0);
-                max_x = cmp::max(max_x, coord.0);
+                *min_x = cmp::min(*min_x, coord.0);
+                *max_x = cmp::max(*max_x, coord.0);
 
-                min_y = cmp::min(min_y, coord.1);
-                max_y = cmp::max(max_y, coord.1);
+                *min_y = cmp::min(*min_y, coord.1);
+                *max_y = cmp::max(*max_y, coord.1);
+            }
+        }
+    }
+}
+
+fn get_neighbours(traversed_rooms: &HashSet<Coord>, room: &Room) -> Vec<Coord> {
+    let mut neighbours = Vec::new();
+
+    for door in &room.doors {
+        let mut coord = room.coord.clone();
+        modify_coord_by_direction(&mut coord, door);
+        if !traversed_rooms.contains(&coord) {
+            neighbours.push(coord);
+        }
+    }
+
+    neighbours
+}
+
+fn main() {
+    let text = read_input::read_text("20/input.txt").unwrap();
+    let text = text.replace("^", "").replace("$", "");
+
+    let mut map: HashMap<Coord, Room> = HashMap::new();
+
+    let mut min_x = 0;
+    let mut min_y = 0;
+
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    setup_map(&text, &mut map, &mut min_x, &mut min_y, &mut max_x, &mut max_y);
+    // draw_map(&map, min_x, min_y, max_x, max_y);
+
+    let mut traversed_rooms: HashSet<Coord> = HashSet::new();
+    traversed_rooms.insert((0, 0));
+
+    // this might be unnecessary. Looks like one can track the paths when they parse the text.
+    // ah well
+    let mut working_stack = vec![((0, 0), vec![(0, 0)])];
+    let mut full_paths = Vec::new();
+    while working_stack.len() > 0 {
+        let (coord, path) = working_stack.remove(0);
+        let room = map.get(&coord).unwrap();
+        traversed_rooms.insert(coord);
+        let neighbours = get_neighbours(&traversed_rooms, room);
+
+        if neighbours.len() == 0 {
+            full_paths.push(path);
+            continue
+        }
+
+        for neighbour in &neighbours {
+            let mut path = path.clone();
+            path.push(*neighbour);
+            working_stack.push((*neighbour, path));
+        }
+    }
+
+    full_paths.sort_by(|a, b| {
+        b.len().cmp(&a.len())
+    });
+
+    println!("{}", full_paths.get(0).unwrap().len() - 1);
+
+    let mut past_1k = HashSet::new();
+    // println!("{}", full_paths.iter().fold(0, |sum, path| {
+    //     if path.len() >= 1000 {
+    //         sum + 1
+    //     } else {
+    //         sum
+    //     }
+    // }));
+
+    for path in &full_paths {
+        if path.len() > 1000 {
+            for coord in path.iter().skip(1000) {
+                past_1k.insert(*coord);
             }
         }
     }
 
-    draw_map(&map, min_x, min_y, max_x, max_y);
+    println!("{}", past_1k.len());
 }
