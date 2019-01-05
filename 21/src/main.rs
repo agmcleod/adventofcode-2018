2faec90b98bda62355c5abf47cc4d3842501eba7
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-01-05 16:52:05 -0500

    added counts

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -4,14 +4,13 @@ fn main() {
     let mut register_zero = 0;
     let mut runs = Vec::with_capacity(1000);
     for _ in 0..1000 {
-        let mut r0: usize = register_zero;
+        let r0: usize = register_zero;
         let mut r1: usize = 0;
         let mut r2: usize = 0;
         let mut r3: usize = 0;
         let mut r4: usize = 0;
-        let mut r5: usize = 0;
 
-        let mut count = 0;
+        let mut count = 6;
 
         println!("Try {}", register_zero);
 
@@ -24,17 +23,28 @@ fn main() {
             r4 = r4 * 65899;
             r4 = r4 & 16777215;
 
+            count += 7;
+
             loop {
+                count += 3;
+                println!("iter");
                 if r3 > 256 {
+                    println!("break");
                     break
                 }
+
+                count += 1;
 
                 r1 = r3 / 256 + 1;
                 r2 = r1 - 1;
 
                 r3 = r2;
+
+                count += r2 * 256 + 3;
+                count += 2; // end of this loop count
             }
 
+            count += 2;
             if r4 == r0 {
                 break
             }
@@ -43,7 +53,6 @@ fn main() {
         runs.push((count, register_zero));
 
         register_zero += 1;
-        break
     }
 
     runs.sort_by(|a, b| {
