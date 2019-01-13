8b8ce02bd93a93868fade1e21cb85b1b2bfc8d76
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-01-13 16:27:37 -0500

    more work on d23p2

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -17,6 +17,15 @@ fn distance(location: &Coord, target: &Coord) -> i64 {
     x_diff + y_diff + z_diff
 }
 
+fn rectangles_intersect(rect1_start: &Coord, rect1_end: &Coord, rect2_start: &Coord, rect2_end: &Coord) -> bool {
+    rect1_start.0 <= rect2_start.0 &&
+    rect1_start.1 <= rect2_start.1 &&
+    rect1_start.2 <= rect2_start.2 &&
+    rect1_end.0 >= rect1_end.0 &&
+    rect1_end.1 >= rect1_end.1 &&
+    rect1_end.2 >= rect1_end.2
+}
+
 fn main() {
     let text = read_input::read_text("23/input.txt").unwrap();
 
@@ -65,21 +74,55 @@ fn main() {
     println!("{}", part_one_count);
     println!("{} {} {} {} {} {}", min_x, min_y, min_z, max_x, max_y, max_z);
 
-    let sub_x_size = (max_x - min_x) / 8;
-    let sub_y_size = (max_y - min_y) / 8;
-    let sub_z_size = (max_z - min_z) / 8;
+    loop {
+        let sub_x_size = (max_x - min_x) / 8;
+        let sub_y_size = (max_y - min_y) / 8;
+        let sub_z_size = (max_z - min_z) / 8;
 
-    let mut highest_count: Vec<(i64, Coord, Coord)> = Vec::new();
+        let mut highest_count: Vec<(i64, Coord, Coord)> = Vec::new();
 
-    for sub_x in 0..8 {
-        for sub_y in 0..8 {
-            for sub_z in 0..8 {
-                let start = ((sub_x * sub_x_size) + min_x, (sub_y * sub_y_size) + min_y, (sub_z * sub_z_size) + min_z);
-                let end = (((sub_x + 1) * sub_x_size) + min_x, ((sub_y + 1) * sub_y_size) + min_y, ((sub_z + 1) * sub_z_size) + min_z);
+        for sub_x in 0..8 {
+            for sub_y in 0..8 {
+                for sub_z in 0..8 {
+                    let start = ((sub_x * sub_x_size) + min_x, (sub_y * sub_y_size) + min_y, (sub_z * sub_z_size) + min_z);
+                    let end = (((sub_x + 1) * sub_x_size) + min_x, ((sub_y + 1) * sub_y_size) + min_y, ((sub_z + 1) * sub_z_size) + min_z);
 
-                for bot in &nanobots {
+                    let mut count = 0;
+
+                    for bot in &nanobots {
+                        let start_bot = (bot.pos.0 - bot.radius, bot.pos.1 - bot.radius, bot.pos.2 - bot.radius);
+                        let end_bot = (bot.pos.0 + bot.radius, bot.pos.1 + bot.radius, bot.pos.2 + bot.radius);
+
+                        if rectangles_intersect(&start, &end, &start_bot, &end_bot) {
+                            count += 1;
+                        }
+                    }
+
+                    highest_count.push((count, start, end));
                 }
             }
         }
+
+        highest_count.sort_by(|a, b| {
+            b.0.cmp(&a.0)
+        });
+
+        let first = highest_count.get(0).unwrap();
+
+        println!("Count collided {}", first.0);
+        
+        min_x = (first.1).0;
+        min_y = (first.1).1;
+        min_z = (first.1).2;
+
+        max_x = (first.2).0;
+        max_y = (first.2).1;
+        max_z = (first.2).2;
+
+        if max_x - min_x <= 1 && max_y - min_y <= 1 && max_z - min_z <= 1 {
+            break
+        }
     }
+
+    println!("{} {} {} {} {} {}", min_x, min_y, min_z, max_x, max_y, max_z);
 }
